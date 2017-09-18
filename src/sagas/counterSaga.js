7ecf155afbc48c5be7c406593a5a659b9113b048
Ho Tri Dung <chris.tridung@gmail.com>
2017-09-19 00:44:13 +0700

    add counter increasing 30 times

--- src/sagas/counterSaga.js
+++ src/sagas/counterSaga.js
@@ -1,12 +1,17 @@
 import {INCREASE, INCREASE_DONE} from "../actions/type";
 import {delay} from 'redux-saga';
-import {takeLatest, put} from 'redux-saga/effects';
+import {takeLatest, takeEvery, throttle, put} from 'redux-saga/effects';
 
 function* incrementAsync() {
-    yield delay(1000);
-    yield put({ type: INCREASE_DONE });
+    yield count = 1;
+    while(count <= 30){
+        yield put({type: INCREASE_DONE});
+        yield delay(30);
+        yield count++;
+    }
 }
 
 export function* watchIncrementAsync() {
     yield takeLatest(INCREASE, incrementAsync);
 }
+
